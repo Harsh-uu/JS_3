6864609b08a30f8d018f5fb12177bd79366cb9b6
Harsh <harshguptaaa17@gmail.com>
2023-02-24 23:16:25 +0530

    destructuring, spread and rest

--- app.js
+++ app.js
@@ -1,23 +1,42 @@
-class Customer {
-    constructor(n){
-        this.name = n;
-    }
+// class Customer {
+//     constructor(n){
+//         this.name = n;
+//     }
 
-    age = 19;
+//     age = 19;
 
-    buy(){
-        console.log("JavaScript God");
-    }
+//     buy(){
+//         console.log("JavaScript God");
+//     }
+// }
+
+// class GuestCustomer extends Customer{
+//     hello(){
+//         console.log("hi");
+//     }
+// }
+
+// let customer1= new GuestCustomer("harsh");
+
+// console.log(customer1);
+
+// customer1.buy();
+
+
+// let list = ["Audi", "Honda", "BMW"];
+
+// let newList = [...list, "Ferrari"];
+
+// console.log(newList);
+
+let person = {
+    name: "Harsh"
 }
 
-class GuestCustomer extends Customer{
-    hello(){
-        console.log("hi");
-    }
+let newPerson = {
+    ...person,
+    city: "Noida"
 }
 
-let customer1= new GuestCustomer("harsh");
 
-console.log(customer1);
-
-customer1.buy();
+console.log(newPerson);
---
+++ hello.js
@@ -0,0 +1,10 @@
+// function hello(...all){
+//     console.log(all);
+// }
+
+// hello(1, 2 ,3);
+let list = ["Audi", "BMW", "Honda"]
+
+let [car1, car2, car3] = list;
+
+console.log(car3);
