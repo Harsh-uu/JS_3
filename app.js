d8a91bf97b810241910cd4010243088f4d655c7d
Harsh <harshguptaaa17@gmail.com>
2023-02-25 23:41:58 +0530

    filter

--- app.js
+++ app.js
@@ -57,10 +57,25 @@
 
 //map
 
+// let array1 = [3, 5, 7, 10];
+
+// let array2 = array1.map( x =>  x*2 );
+
+// console.log(array2);
+
+//filter
+
 let array1 = [2, 5, 7, 10];
 
-let array2 = array1.map(function(x){
-    return x*2;
-});
+let array2 = array1.filter( x =>  x%2 == 0 );
 
+console.log(array1);
 console.log(array2);
+
+let array3 = array1.filter(function(x){
+    if(x%5 == 0){
+        return x;
+    }
+})
+
+console.log(array3);
